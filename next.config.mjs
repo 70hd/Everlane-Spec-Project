068be684eda5736dc4734bcd0235da2261a89063
70hd <kanehfernandez@gmail.com>
2025-04-27 14:44:36 -0700

    add all files

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
     images: {
         domains: ['us.sunspel.com', 'media.everlane.com'],
       },
