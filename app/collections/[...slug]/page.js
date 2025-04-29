c33f6616bc907e2c23a9801861bf20c004e33406
70hd <kanehfernandez@gmail.com>
2025-04-28 21:30:17 -0700

    domain fix

--- app/collections/[...slug]/page.js
+++ app/collections/[...slug]/page.js
@@ -26,7 +26,7 @@ const Page = () => {
       };
     }, {});
   }, [products]);
-  const LOCALHOST_PREFIX = "https://everlane-spec-project.vercel.app//collections/";
+  const LOCALHOST_PREFIX = "https://everlane-spec-project.vercel.app/collections/";
 
   useEffect(() => {
     setHeart(createHeartState);
